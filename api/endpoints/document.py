064d5ce9f968f746ac22d47cc1bbeb4f104292cc
Wasa-B <ghd1876@gmail.com>
2025-08-28 17:55:52 +0900

    요구사항정의서 삭제 조회 api

--- api/endpoints/document.py
+++ api/endpoints/document.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException, Request
-from schemas.document import ProductRequirementsDocumentRequest, ProductRequirementsDocument
+from schemas.document import ProductRequirementsDocumentRequest, ProductRequirementsDocument,DocumentIdResponse
 from ai.product_requirement_document.prd_generator import generate_document
 from mongo_db import init_db, db
 import asyncio
+from bson import ObjectId
+from typing import List
 router = APIRouter()
 
 async def generate_requirement_document(requirement, db, db_object_id):
@@ -12,16 +14,47 @@ async def generate_requirement_document(requirement, db, db_object_id):
           {"_id":db_object_id},
           {"$set":{"document":document.model_dump(), "status":"finished"}}
       )
+      print("generate finish")
 
-@router.post("/", response_model=ProductRequirementsDocument)
+@router.post("/", response_model=DocumentIdResponse)
 async def create_product_requirement_document(requirement: ProductRequirementsDocumentRequest, request: Request):
     db = request.app.state.db
     db_data = ProductRequirementsDocument(owner_id=requirement.owner_id, project_id=requirement.project_id,status="progress")
     db_object = await db.requirement_document.insert_one(db_data.model_dump())
+    
     db_object_id = db_object.inserted_id
     # print(requirement)
-
-    
     asyncio.create_task(generate_requirement_document(requirement.requirement,db,db_object_id))
-    
-    return db_data
+    result = DocumentIdResponse(document_id=str(db_object_id))
+    return result
+
+@router.get("/requirement/project/{project_id}",response_model=List[ProductRequirementsDocument])
+async def get_product_requirement_document(project_id: str, request : Request): 
+    """
+        프로젝트 id를 기반으로 요구사항정의서 목록을 mongodb에서 찾음
+    """
+    db = request.app.state.db
+    documents = await db.requirement_document.find({"project_id":project_id}).to_list()
+    if documents :
+         return [ProductRequirementsDocument(**document) for document in documents]
+    return None
+
+@router.get("/requirement/{id}", response_model=ProductRequirementsDocument)
+async def get_product_requirement_document(id: str, request : Request):
+    """
+    문서id를 기반으로 요구사항정의서를 mongodb에서 찾음
+    """
+    db = request.app.state.db
+    document = await db.requirement_document.find_one({"_id":ObjectId(id)})
+    if document :
+         return ProductRequirementsDocument(**document)
+    return ProductRequirementsDocument()
+
+@router.delete("/requirement/{id}")
+async def get_product_requirement_document(id: str, request : Request): 
+    """
+    문서id를 기반으로 요구사항정의서를 mongodb에서 삭제
+    """
+    db = request.app.state.db
+    result  = await db.requirement_document.delete_one({"_id":ObjectId(id)})
+    return {"msg":f"delete {result.deleted_count}"}
--- schemas/document.py
+++ schemas/document.py
@@ -1,6 +1,7 @@
 from ai.product_requirement_document.prd_generator import Requirement_Document
 from pydantic import BaseModel, Field
 from typing import Optional, List
+from datetime import datetime, timezone
 
 class ProductRequirementsDocumentRequest(BaseModel):
   owner_id : str
@@ -8,7 +9,11 @@ class ProductRequirementsDocumentRequest(BaseModel):
   requirement : str
 
 class ProductRequirementsDocument(BaseModel):
-  owner_id : str
-  project_id : str
-  status : str = Field(...,examples="finish")
+  owner_id : str= Field(...,example="owner_user_id")
+  project_id : str = Field(...,example="project_id")
+  status : str = Field(...,example="finish")
+  create_date : datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
   document : Requirement_Document = None
+
+class DocumentIdResponse(BaseModel):
+  document_id : str
